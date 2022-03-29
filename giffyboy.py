c1c8285aa9a5828f22aa6ec4e35ebca9768d63f3
dylan <dylan@dylanreed.com>
2022-03-29 12:31:56 -0600

    What in the world

--- giffyboy.py
+++ giffyboy.py
@@ -1,13 +1,37 @@
-import os
+import PIL
 import imageio
-from pygifsicle import optimize
+from PIL import Image
+import os, sys
 
-png_dir = 'images'
+
+path = "/Users/nervousmini/Documents/GitHub/peaceful-unicorns/images/"
+
+resize_ratio = 0.5  # where 0.5 is half size, 2 is double size
+
+def resize_aspect_fit():
+    dirs = os.listdir(path)
+    for item in dirs:
+        if item == '.png':
+            continue
+        if os.path.isfile(path+item):
+            image = Image.open(path+item)
+            file_path, extension = os.path.splitext(path+item)
+
+            new_image_height = int(image.size[0] / (1/resize_ratio))
+            new_image_length = int(image.size[1] / (1/resize_ratio))
+
+            image = image.resize((new_image_height, new_image_length), Image.ANTIALIAS)
+            image.save("/Users/nervousmini/Documents/GitHub/peaceful-unicorns/images/resized/" + item + "_small" + extension, 'JPEG', quality=90)
+
+
+resize_aspect_fit()
+
+
+
+small_dir = '/Users/nervousmini/Documents/GitHub/peaceful-unicorns/images/resized'
 images = []
-for file_name in sorted(os.listdir(png_dir)):
+for file_name in sorted(os.listdir(small_dir)):
     if file_name.endswith('.png'):
-        file_path = os.path.join(png_dir, file_name)
+        file_path = os.path.join(small_dir, file_name)
         images.append(imageio.imread(file_path))
-imageio.mimsave('gif/movie.gif', images)
-
-optimize('gif/movie.gif')
+imageio.mimsave('/Users/nervousmini/Documents/GitHub/peaceful-unicorns/gif/movie.gif', images, fps = 3)
--- twister.py
+++ twister.py
@@ -8,161 +8,169 @@ import json
 # Each image is made up a series of traits
 # The weightings for each trait drive the rarity and add up to 100%
 
-#background = ["balloon", "basket case", "black and blue", "nervous", "orange is the new green", "pretty", "too bright"]  
-#background_weights =  [15, 15, 14, 14, 14, 14, 14]
+background = ["Diagonal","Diagonal with Stars","Horizontal","Horizontal with Stars","Spiral 1","Spiral 1 with Stars","Spiral 2","Spiral 2 with Stars","Spiral 3","Spiral 3 with Stars","Vertical","Vertical with Stars"] 
+background_weights = [0.09, 0.08, 0.09, 0.08, 0.09, 0.08, 0.09, 0.08, 0.08, 0.08, 0.08, 0.08]
 
-body = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-body_weights = [14, 15, 15, 14, 14, 14, 14]
+body = ["Blue","Green","Grey","Indigo","Orange","Red","Violet","Yellow"] 
+body_weights = [0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125]
 
-leftbackleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-leftbackleg_weights = [14, 14, 15, 15, 14, 14, 14]
+cloud = ["Yes","No"] 
+cloud_weights = [0.1, 0.9]
 
-leftear = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-leftear_weights = [14, 14, 14, 15, 15, 14, 14]
+stars = ["None","Even More","More","Some"] 
+stars_weights = [0.90, 0.06, 0.03, 0.1]
 
-leftfrontleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-leftfrontleg_weights = [14, 14, 14, 14, 15, 15, 14]
+spots = ["Blank","Bright Pink","Dark Purple","Green Shimmer","Green Shine","Green Tourquise Shimmer","Green","Pearlescence","Pink","Pinkish Orange","Pretty Purple","Purplish Blue","Royal Purple","Salmon","Shimmer","Sky Blue","Yellow Green","Aqua Green"] 
+spots_weights = [0.6, 0.0375, 0.0375, 0.0375, 0.0375, 0.0375, 0.0375, 0.0111, 0.0375, 0.0376, 0.0111, 0.0111, 0.0111, 0.0111, 0.0111, 0.0111, 0.0111, 0.0111]
 
-neck = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-neck_weights = [14, 14, 14, 14, 14, 15, 15]
+horn = ["Big Rainbow","Small Rainbow","Ukraine Stripes","Ukraine"] 
+horn_weights = [0.2, 0.2, 0.3, 0.3]
 
-nose = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-nose_weights = [15, 14, 14, 14, 14, 14, 15]
+tail = ["Long Rainbow","Long Ukraine","Short Rainbow","Short Ukraine","Speedy Rainbow","Speedy Ukraine"] 
+tail_weights = [0.15, 0.18, 0.15, 0.18, 0.15, 0.19]
 
-rightbackleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-rightbackleg_weights = [15, 15, 14, 14, 14, 14, 14]
+mane = ["Blue Spikes","Curly Rainbow","Curly Ukraine","Lime Spikes","Orange Spikes","Pink Spikes","Purple Spikes","Rainbow Updo","Red Spikes","Short Rainbow","Short Ukraine","Sleek Rainbow","Sleek Ukraine","Ukraine Updo","Yellow Spikes"] 
+mane_weights = [0.06, 0.07, 0.07, 0.06, 0.06, 0.06, 0.06, 0.07, 0.06, 0.07, 0.07, 0.07, 0.08, 0.08, 0.06]
 
-rightear = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] 
-rightear_weights = [14, 15, 15, 14, 14, 14, 14]
+eyes = ["Half Closed","Wide Awake","Wink"]  
+eyes_weights = [0.1, 0.8, 0.1]
 
-rightfrontleg = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]  
-rightfrontleg_weights = [14, 14, 15, 15, 14, 14, 14]
+mouth = ["Happy","Hmm","Oh","Smile"]  
+mouth_weights = [0.4, 0.1, 0.1, 0.4]
 
-tail = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]  
-tail_weights = [14, 14, 14, 15, 15, 14, 14]
+flash = ["Banana","None","Cream","Lavender","Mint","Peach","White"]  
+flash_weights = [0.05, 0.7, 0.05, 0.05, 0.05, 0.05, 0.05]
 
-outline = ["black"]  
-outline_weights = [100]
+blush = ["Dark","Medium","Light","None"]  
+blush_weights = [0.1, 0.1, 0.1, 0.7]
 
 # Dictionary variable for each trait. 
 # Eech trait corresponds to its file name
 # Add more shapes and colours as you wish
 
-#background_files = {
-#    "balloon": "balloon",
-#    "basket case": "basket-case",
-#    "black and blue": "black-blue",
-#    "nervous": "nervous",
-#    "orange is the new green": "orange-green",
-#    "pretty": "pretty",
-#    "too bright": "too-bright"
-#}
+background_files = {
+    "Diagonal":"diagonal",
+    "Diagonal with Stars":"diagonal_stars",
+    "Horizontal":"horizontal",
+    "Horizontal with Stars":"horizontal_stars",
+    "Spiral 1":"spiral_1",
+    "Spiral 1 with Stars":"spiral_1_stars",
+    "Spiral 2":"spiral_2",
+    "Spiral 2 with Stars":"spiral_2_stars",
+    "Spiral 3":"spiral_3",
+    "Spiral 3 with Stars":"spiral_3_stars",
+    "Vertical":"vertical",
+    "Vertical with Stars":"vertical_stars"
+}
 
 body_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+    "Blue":"blue",
+    "Green":"green",
+    "Grey":"grey",
+    "Indigo":"indigo",
+    "Orange":"orange",
+    "Red":"red",
+    "Violet":"violet",
+    "Yellow":"yellow"
 }
 
-leftbackleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+cloud_files = {
+    "Yes":"cloud",
+    "No":"blank"
 }
 
-leftear_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+stars_files = {
+    "None":"blank",
+    "Even More":"even_more_stars",
+    "More":"more_stars",
+    "Some":"stars"
 }
 
-leftfrontleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+spots_files = {
+    "Blank":"blank",
+    "Bright Pink":"bright_pink",
+    "Dark Purple":"dark_purple",
+    "Green Shimmer":"green_shimmer",
+    "Green Shine":"green_shine",
+    "Green Tourquise Shimmer":"green_tourquise_shimmer",
+    "Green":"green",
+    "Pearlescence":"pearlescence",
+    "Pink":"pink",
+    "Pinkish Orange":"pinkish_orange",
+    "Pretty Purple":"pretty_purple",
+    "Purplish Blue":"purplish_blue",
+    "Royal Purple":"royal_purple",
+    "Salmon":"salmon",
+    "Shimmer":"shimmer",
+    "Sky Blue":"sky_blue",
+    "Yellow Green":"yellow_green",
+    "Aqua Green":"aqua_green"
 }
 
-neck_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-nose_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-outline_files = {
-    "black": "black",
-    #"grey": "grey",
-    "white": "white"
-}
-
-rightbackleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-rightear_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
-}
-
-rightfrontleg_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+horn_files = {
+    "Big Rainbow":"big_rainbow",
+    "Small Rainbow":"small_rainbow",
+    "Ukraine Stripes":"ukraine_stripes",
+"Ukraine":"ukraine"
 }
 
 tail_files = {
-    "red": "red",
-    "orange": "orange",
-    "yellow": "yellow",
-    "green": "green",
-    "blue": "blue",
-    "indigo": "indigo",
-    "violet": "violet"
+    "Long Rainbow":"long_rainbow",
+    "Long Ukraine":"long_ukraine",
+    "Short Rainbow":"short_rainbow",
+    "Short Ukraine":"short_ukraine",
+    "Speedy Rainbow":"speedy_rainbow",
+    "Speedy Ukraine":"speedy_ukraine"
 }
+
+mane_files = {
+    "Blue Spikes":"blue_spikes",
+    "Curly Rainbow":"curly_rainbow",
+    "Curly Ukraine":"curly_ukraine",
+    "Lime Spikes":"lime_spikes",
+    "Orange Spikes":"orange_spikes",
+    "Pink Spikes":"pink_spikes",
+    "Purple Spikes":"purple_spikes",
+    "Rainbow Updo":"rainbow_updo",
+    "Red Spikes":"red_spikes",
+    "Short Rainbow":"short_rainbow",
+    "Short Ukraine":"short_ukraine",
+    "Sleek Rainbow":"sleek_rainbow",
+    "Sleek Ukraine":"sleek_ukraine",
+    "Ukraine Updo":"ukraine_updo",
+    "Yellow Spikes":"yellow_spikes"
+}
+
+eyes_files = {
+    "Half Closed":"half_closed",
+    "Wide Awake":"wide_awake",
+    "Wink":"wink"
+}
+
+mouth_files = {
+    "Happy":"happy",
+    "Hmm":"hmm",
+    "Oh":"oh",
+    "Smile":"smile"
+}
+
+flash_files = {
+    "Banana":"banana",
+    "None":"blank",
+    "Cream":"cream",
+    "Lavender":"lavender",
+    "Mint":"mint",
+    "Peach":"peach",
+    "White":"white"
+}
+
+blush_files = {
+    "Dark":"dark",
+    "Medium":"medium",
+    "Light":"light",
+    "None":"blank"
+}
+
 #Create a function to generate unique image combinations
 TOTAL_IMAGES = 100 # Number of random unique images we want to generate ( 2 x 2 x 2 = 8)
 
@@ -173,18 +181,18 @@ def create_new_image():
     new_image = {} #
 
     # For each trait category, select a random trait based on the weightings 
-    #new_image ["Background"] = random.choices(background, background_weights)[0]
+    new_image ["Background"] = random.choices(background, background_weights)[0]
     new_image ["Body"] = random.choices(body, body_weights)[0]
-    new_image ["Left Back Leg"] = random.choices(leftbackleg, leftbackleg_weights)[0]
-    new_image ["Left Ear"] = random.choices(leftear, leftear_weights)[0]
-    new_image ["Left Front Leg"] = random.choices(leftfrontleg, leftfrontleg_weights)[0]
-    new_image ["Neck"] = random.choices(neck, neck_weights)[0]
-    new_image ["Nose"] = random.choices(nose, nose_weights)[0]
-    new_image ["Outline"] = random.choices(outline, outline_weights)[0]
-    new_image ["Right Back Leg"] = random.choices(rightbackleg, rightbackleg_weights)[0]
-    new_image ["Right Ear"] = random.choices(rightear, rightear_weights)[0]
-    new_image ["Right Front Leg"] = random.choices(rightfrontleg, rightfrontleg_weights)[0]
+    new_image ["Cloud"] = random.choices(cloud, cloud_weights)[0]
+    new_image ["Stars"] = random.choices(stars, stars_weights)[0]
+    new_image ["Spots"] = random.choices(spots, spots_weights)[0]
+    new_image ["Horn"] = random.choices(horn, horn_weights)[0]
+    new_image ["Flash"] = random.choices(flash, flash_weights)[0]
     new_image ["Tail"] = random.choices(tail, tail_weights)[0]
+    new_image ["Mane"] = random.choices(mane, mane_weights)[0]
+    new_image ["Eyes"] = random.choices(eyes, eyes_weights)[0]
+    new_image ["Mouth"] = random.choices(mouth, mouth_weights)[0]
+    new_image ["Blush"] = random.choices(blush, blush_weights)[0]
 
 
     if new_image in all_images:
@@ -221,81 +229,81 @@ print(all_images)
 
 #get trait count
 
-#background_count = {}
-#for item in background:
-#    background_count[item] = 0
+background_count = {}
+for item in background:
+    background_count[item] = 0
+
+blush_count = {}
+for item in blush:
+    blush_count[item] = 0
 
 body_count = {}
 for item in body:
     body_count[item] = 0
 
-leftbackleg_count = {}
-for item in leftbackleg:
-    leftbackleg_count[item] = 0
+cloud_count = {}
+for item in cloud:
+    cloud_count[item] = 0
 
-leftear_count = {}
-for item in leftear:
-    leftear_count[item] = 0
+stars_count = {}
+for item in stars:
+    stars_count[item] = 0
 
-leftfrontleg_count = {}
-for item in leftfrontleg:
-    leftfrontleg_count[item] = 0
+spots_count = {}
+for item in spots:
+    spots_count[item] = 0
 
-neck_count = {}
-for item in neck:
-    neck_count[item] = 0
+horn_count = {}
+for item in horn:
+    horn_count[item] = 0
 
-nose_count = {}
-for item in nose:
-    nose_count[item] = 0
-
-outline_count = {}
-for item in outline:
-    outline_count[item] = 0
-
-rightbackleg_count = {}
-for item in rightbackleg:
-    rightbackleg_count[item] = 0
-
-rightear_count = {}
-for item in rightear:
-    rightear_count[item] = 0
-
-rightfrontleg_count = {}
-for item in rightfrontleg:
-    rightfrontleg_count[item] = 0
+flash_count = {}
+for item in flash:
+    flash_count[item] = 0
 
 tail_count = {}
 for item in tail:
     tail_count[item] = 0
 
+mane_count = {}
+for item in mane:
+    mane_count[item] = 0
+
+eyes_count = {}
+for item in eyes:
+    eyes_count[item] = 0
+
+mouth_count = {}
+for item in mouth:
+    mouth_count[item] = 0
+
 for image in all_images:
-    #background_count[image["Background"]] += 1
+    blush_count[image["Blush"]] += 1
+    background_count[image["Background"]] += 1
     body_count[image["Body"]] += 1
-    leftbackleg_count[image["Left Back Leg"]] += 1
-    leftear_count[image["Left Ear"]] += 1
-    leftfrontleg_count[image["Left Front Leg"]] += 1
-    neck_count[image["Neck"]] += 1
-    nose_count[image["Nose"]] += 1
-    outline_count[image["Outline"]] += 1
-    rightbackleg_count[image["Right Back Leg"]] += 1
-    rightear_count[image["Right Ear"]] += 1
-    rightfrontleg_count[image["Right Front Leg"]] += 1
+    cloud_count[image["Cloud"]] += 1
+    stars_count[image["Stars"]] += 1
+    spots_count[image["Spots"]] += 1
+    horn_count[image["Horn"]] += 1
+    flash_count[image["Flash"]] += 1
     tail_count[image["Tail"]] += 1
+    mane_count[image["Mane"]] += 1
+    eyes_count[image["Eyes"]] += 1
+    mouth_count[image["Mouth"]] += 1
 
 
-#print(background_count)
+print(blush_count)
+print(background_count)
 print(body_count)
-print(leftbackleg_count)
-print(leftear_count)
-print(leftfrontleg_count)
-print(neck_count)
-print(nose_count)
-print(outline_count)
-print(rightbackleg_count)
-print(rightear_count)
-print(rightfrontleg_count)
+print(cloud_count)
+print(stars_count)
+print(spots_count)
+print(horn_count)
+print(flash_count)
 print(tail_count)
+print(mane_count)
+print(eyes_count)
+print(mouth_count)
 
 
 #Generate Metadata for all Traits
@@ -309,35 +317,38 @@ with open(METADATA_FILE_NAME, 'w') as outfile:
 
 for item in all_images:
 
-    #im1 = Image.open(f'./layers/background/{background_files[item["Background"]]}.png').convert('RGBA')
-    im2 = Image.open(f'./layers/body/{body_files[item["Body"]]}.png').convert('RGBA')
-    im3 = Image.open(f'./layers/left back leg/{leftbackleg_files[item["Left Back Leg"]]}.png').convert('RGBA')
-    im4 = Image.open(f'./layers/left ear/{leftear_files[item["Left Ear"]]}.png').convert('RGBA')
-    im5 = Image.open(f'./layers/left front leg/{leftfrontleg_files[item["Left Front Leg"]]}.png').convert('RGBA')
-    im6 = Image.open(f'./layers/neck/{neck_files[item["Neck"]]}.png').convert('RGBA')
-    im7 = Image.open(f'./layers/nose/{nose_files[item["Nose"]]}.png').convert('RGBA')
-    im8 = Image.open(f'./layers/right back leg/{tail_files[item["Tail"]]}.png').convert('RGBA')
-    im9 = Image.open(f'./layers/right ear/{rightbackleg_files[item["Right Back Leg"]]}.png').convert('RGBA')
-    im10 = Image.open(f'./layers/right front leg/{rightear_files[item["Right Ear"]]}.png').convert('RGBA')
-    im11 = Image.open(f'./layers/tail/{rightfrontleg_files[item["Right Front Leg"]]}.png').convert('RGBA')
-    im12 = Image.open(f'./layers/outline/{outline_files[item["Outline"]]}.png').convert('RGBA')
 
+    im1 = Image.open(f'./layers/background/{background_files[item["Background"]]}.png').convert('RGBA')
+    im2 = Image.open(f'./layers/body/{body_files[item["Body"]]}.png').convert('RGBA')
+    im3 = Image.open(f'./layers/cloud/{cloud_files[item["Cloud"]]}.png').convert('RGBA')
+    im4 = Image.open(f'./layers/stars/{stars_files[item["Stars"]]}.png').convert('RGBA')
+    im5 = Image.open(f'./layers/spots/{spots_files[item["Spots"]]}.png').convert('RGBA')
+    im6 = Image.open(f'./layers/horn/{horn_files[item["Horn"]]}.png').convert('RGBA')
+    im7 = Image.open(f'./layers/mouth/{mouth_files[item["Mouth"]]}.png').convert('RGBA')
+    im8 = Image.open(f'./layers/tail/{tail_files[item["Tail"]]}.png').convert('RGBA')
+    im9 = Image.open(f'./layers/mane/{mane_files[item["Mane"]]}.png').convert('RGBA')
+    im10 = Image.open(f'./layers/eyes/{eyes_files[item["Eyes"]]}.png').convert('RGBA')
+    im11 = Image.open(f'./layers/flash/{flash_files[item["Flash"]]}.png').convert('RGBA')
+    im12 = Image.open(f'./layers/blush/{blush_files[item["Blush"]]}.png').convert('RGBA')
+    
     #Create each composite
-    com1 = Image.alpha_composite(im2, im3)
-    com2 = Image.alpha_composite(com1, im4)
-    com3 = Image.alpha_composite(com2, im5)
-    com4 = Image.alpha_composite(com3, im6)
-    com5 = Image.alpha_composite(com4, im7)
-    com6 = Image.alpha_composite(com5, im8)
-    com7 = Image.alpha_composite(com6, im9)
-    com8 = Image.alpha_composite(com7, im10)
-    com9 = Image.alpha_composite(com8, im11)
-    com10 = Image.alpha_composite(com9, im12)
-    #com11 = Image.alpha_composite(com10, im12)
+    com1 = Image.alpha_composite(im1, im2)
+    com2 = Image.alpha_composite(com1, im3)
+    com3 = Image.alpha_composite(com2, im4)
+    com4 = Image.alpha_composite(com2, im5)
+    com5 = Image.alpha_composite(com4, im6)
+    com6 = Image.alpha_composite(com5, im7)
+    com7 = Image.alpha_composite(com6, im8)
+    com8 = Image.alpha_composite(com7, im9)
+    com9 = Image.alpha_composite(com8, im10)
+    com10 = Image.alpha_composite(com9, im11)
+    com11 = Image.alpha_composite(com10, im12)
+
     #Convert to RGB
-    rgb_im = com10.convert('RGB')
+    rgb_im = com11.convert('RGB')
     file_name = str(item["tokenId"]) + ".png"
     rgb_im.save("./images/" + file_name)
+    print (file_name)
 
 #Generate Metadata
 
@@ -362,13 +373,13 @@ for item in all_images:
 #    }
 #    token["attributes"].append(getAttribute("Background", i["Background"]))
 #    token["attributes"].append(getAttribute("Back Legs", i["Back Legs"]))
-#    token["attributes"].append(getAttribute("Body", i["Body"]))
+#    token["attributes"].append(getAttribute("background", i["background"]))
 #    token["attributes"].append(getAttribute("Ears", i["Ears"]))
 #    token["attributes"].append(getAttribute("Front Legs", i["Front Legs"]))
 #    token["attributes"].append(getAttribute("Head", i["Head"]))
-#    token["attributes"].append(getAttribute("Neck", i["Neck"]))
-#    token["attributes"].append(getAttribute("Tail", i["Tail"]))
-#    token["attributes"].append(getAttribute("Outline", i["Outline"]))
+#    token["attributes"].append(getAttribute("spots", i["spots"]))
+#    token["attributes"].append(getAttribute("mouth", i["mouth"]))
+#    token["attributes"].append(getAttribute("flash", i["flash"]))
 
 #    with open('./metadata/' + str(token_id), 'w') as outfile:
 #        json.dump(token, outfile, indent=4)
